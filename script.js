62fe9651728ecd5230d7683442f4294ec3c1dccd
Sky28702 <prateekrai462@outlook.com>
2025-06-01 03:15:01 +0530

    tryunc

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World !");
+let sex = prompt("You love momo?");
+if (sex == "yes") {
+  console.log(sex + " ! You are Nepali");
+} else if (sex == "no") {
+  console.log(sex + " ! You are not Nepali");
+} else {
+  console.log("Answer in yes or no , lowecases");
+}
