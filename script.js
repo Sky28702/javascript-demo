7e6c6af8939da4b36f3430919d14ee84d0a5bfef
Sky28702 <prateekrai462@outlook.com>
2025-06-02 19:50:38 +0530

    Javamore

--- script.js
+++ script.js
@@ -1,96 +1,49 @@
-//Array
-let Array = ["maybe", "yes", "no"];
-console.log(Array.length);
-
-//Loop
-
-for (i = 0; i < 6; i++) {
-  alert("do not reddemeee it madammmm", i);
-}
-
-//Else if prompt
-
-let sex = prompt("You love momo?");
-sex = sex.toLowerCase();
-sex = sex.trim();
-if (sex == "yes") {
-  alert(sex + " ! You are Nepali");
-} else if (sex == "no") {
-  alert(sex + " ! You are not Nepali");
-}
-
-// Objects
-
-let bio = { Sniper: "no scope", Pistol: "xfive", Throw: "Shuriken" };
-alert(bio.Sniper + " Bio use it");
-
-//functions
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* array */
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(array);
+console.log(array[4]);
+
+/* Objects */
+let Pc = {
+  Ram: "8GB",
+  CPU: "12 Cores",
+  Graphics_Card: "3050 RTX",
+  OS: "Windows",
+};
+console.log(Pc.CPU, Pc.Graphics_Card);
+
+/* Array of objects*/
+
+const Arrays = [
+  {
+    id: 1,
+    firstName: "Prateek",
+    lastName: "Rai",
+    email: "prateekrai462@outlook.com",
+    age: 19,
+  },
+
+  {
+    id: 2,
+    firstName: "San",
+    lastName: "Jaya",
+    email: "xyz@outlook.com",
+    age: 57,
+  },
+];
+
+console.log(Arrays);
+console.log(Arrays[0]);
+
+/* For each loop*/
+Arrays.forEach(function (data, index) {
+  console.log(
+    index +
+      ". My name is  " +
+      data.firstName +
+      data.lastName +
+      " and i am " +
+      data.age +
+      " years old."
+  );
+});
