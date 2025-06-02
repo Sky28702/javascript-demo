f59798f0197a59cb825dfd887fe993bc38f0f266
Sky28702 <prateekrai462@outlook.com>
2025-06-02 15:13:37 +0530

    Jsmore

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+//Array
+let Array = ["maybe", "yes", "no"];
+console.log(Array.length);
+
+//Loop
+
+for (i = 0; i < 6; i++) {
+  alert("do not reddemeee it madammmm", i);
+}
+
+//Else if prompt
+
 let sex = prompt("You love momo?");
 sex = sex.toLowerCase();
 sex = sex.trim();
@@ -6,3 +18,79 @@ if (sex == "yes") {
 } else if (sex == "no") {
   alert(sex + " ! You are not Nepali");
 }
+
+// Objects
+
+let bio = { Sniper: "no scope", Pistol: "xfive", Throw: "Shuriken" };
+alert(bio.Sniper + " Bio use it");
+
+//functions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
